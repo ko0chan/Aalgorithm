f4c9d1d960ad2815e18e13c4b099140c89b3ba41
ko0chan <58040559+ko0chan@users.noreply.github.com>
2020-04-07 23:46:06 +0900

    체육수업을 들을 수 있는 학생 최대 수 출력

---
+++ 007. 체육복/Gym_suit.kt
@@ -0,0 +1,39 @@
+class Solution {
+    //    fun temp(x: IntArray, y: IntArray) = when(x){
+//        lost
+//    }
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = 0
+        var array = IntArray(n) {0}
+        var cnt = 0
+
+
+        for (lost in lost) --array[lost-1]
+        for (reserve in reserve) ++array[reserve-1]
+
+        for (i in 1 until n) {
+            if (array[i - 1] + array[i] == 0) {
+                array[i - 1] = 0
+                array[i] = 0
+            }
+        }
+        for (i in 0 until n){
+            if(array[i] >= 0 )
+                cnt++
+        }
+        answer = cnt
+        print(answer)
+
+        return answer
+    }
+}
+
+
+fun main(args: Array<String>) {
+    var n = 5
+    var lost = intArrayOf(2,4)
+    var reserve = intArrayOf(1,3,5)
+
+    Solution().solution(n,lost, reserve)
+
+}
