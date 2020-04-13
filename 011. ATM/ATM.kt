cb83976e965b02bfe45ecf7fff1a16c206b09682
ko0chan <58040559+ko0chan@users.noreply.github.com>
2020-04-13 21:36:54 +0900

    split(), map() 이용

--- 011. ATM/ATM.kt
+++ 011. ATM/ATM.kt
@@ -1,27 +1,15 @@
-import java.util.*
-//백준 ATM 11399번 https://www.acmicpc.net/problem/11399
 fun main(args: Array<String>) {
-    var P_num = readLine()!!.toInt()
-//    var P_time = Array<Int>(P_num){ readLine()!!.toInt()}.sorted()
-    var P_time = Array<Int>(P_num){0}
-    val input = Scanner(System.`in`)
-    for (i in 0 until P_num){
-        P_time[i] = input.nextInt()
-    }
 
-    P_time.sort()
+    var P_num = readLine()!!.toInt()
+    var time = (readLine()!!.split(" ")).map(String::toInt).sorted()
 
     var sum = 0
     var result = 0
+
     for (i in 0 until P_num) {
-        sum += P_time[i]
+        sum += time[i]
         result += sum
     }
-//    for (i in P_num downTo 1) {
-//        for (j in 0 .. P_time.size - i) {
-//            result += P_time[j]
-//    }
-
-
     print(result)
+
 }
