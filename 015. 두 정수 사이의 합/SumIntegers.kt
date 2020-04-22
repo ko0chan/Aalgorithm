521d30ad4cb536955f76e372f170a2755e5cbb38
ko0chan <58040559+ko0chan@users.noreply.github.com>
2020-04-23 01:01:48 +0900

    두 정수 사이의 합 구현

---
+++ 015. 두 정수 사이의 합/SumIntegers.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun solution(a: Int, b: Int): Long {
+        var answer: Long = 0
+
+        if (a<=b){
+            for (i in a..b){
+              answer += i
+            }
+        }
+        else{
+            for (i in b..a){
+                answer+= i
+            }
+        }
+
+        return answer
+    }
+}
+
+fun main(args: Array<String>) {
+    var a=3
+    var b=5
+
+    Solution().solution(a,b)
+
+}
