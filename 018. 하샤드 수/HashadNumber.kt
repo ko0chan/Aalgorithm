09552b6a981c40fc1dc54b6dec7a03b9cf0651a4
ko0chan <58040559+ko0chan@users.noreply.github.com>
2020-04-28 16:11:13 +0900

    하샤드 수 구현

---
+++ 018. 하샤드 수/HashadNumber.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(x: Int): Boolean {
+        var stringInt = x.toString()
+        var sum: Int = 0
+        for (i in stringInt) {
+            sum += i.toInt()-48
+        }
+        
+        return x % sum == 0
+    }
+}
+
+fun main(args: Array<String>) {
+    var x = 10
+    Solution().solution(x)
+}
